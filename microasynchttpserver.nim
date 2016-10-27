dd5884d717ecb5e94fc8e3b2a8c462e2e78e4c84
Philip Wernersbach <philip.wernersbach@gmail.com>
2016-10-27 13:02:45 -0400

    Implement Nim 0.15.0 compatibility.

--- microasynchttpserver.nim
+++ microasynchttpserver.nim
@@ -45,6 +45,11 @@ proc newMicroAsyncHttpServer*(): MicroAsyncHttpServer =
     new(result)
     result.socket = newAsyncSocket()
 
+proc sendBadRequestAndClose(socket: AsyncSocket): Future[void] {.async.} =
+    if not socket.isClosed:
+        await socket.send("HTTP/1.0 400 Bad Request\r\nExpires: Thu, 01 Jan 1970 00:00:01 GMT\r\nContent-Length: 0\r\n\r\n")
+        socket.close
+
 proc processConnection(socket: AsyncSocket, hostname: string, callback: proc (request: Request): Future[void] {.closure,gcsafe.}) {.async.} =
     while not socket.isClosed:
         var httpMethod: string
@@ -75,15 +80,41 @@ proc processConnection(socket: AsyncSocket, hostname: string, callback: proc (re
         var headers = newSeq[phr_header](numberOfHeaders)
         
         if tryParseRequest(headerBuffer, httpMethod, path, minorVersion, headers) >= 0:
-            var request = Request(client: socket, reqMethod: httpMethod.toLower, headers: headers,
-                                  protocol: (orig: "HTTP/1." & $minorVersion, major: 1, minor: int(minorVersion)), 
-                                  url: parseUri(path), hostname: hostname, body: "")
+            var reqMethodIsValid = true
 
-            await callback(request)
+            let reqMethod = case httpMethod.toUpper
+                of "GET":
+                    HttpGet
+                of "POST":
+                    HttpPost
+                of "HEAD":
+                    HttpHead
+                of "PUT":
+                    HttpPut
+                of "DELETE":
+                    HttpDelete
+                of "TRACE":
+                    HttpTrace
+                of "OPTIONS":
+                    HttpOptions
+                of "CONNECT":
+                    HttpConnect
+                of "PATCH":
+                    HttpPatch
+                else:
+                    reqMethodIsValid = false
+                    HttpGet
+
+            if reqMethodIsValid:
+                var request = Request(client: socket, reqMethod: reqMethod, headers: headers,
+                                      protocol: (orig: "HTTP/1." & $minorVersion, major: 1, minor: int(minorVersion)), 
+                                      url: parseUri(path), hostname: hostname, body: "")
+
+                await callback(request)
+            else:
+                await socket.sendBadRequestAndClose
         else:
-            if not socket.isClosed:
-                await socket.send("HTTP/1.0 400 Bad Request\r\nExpires: Thu, 01 Jan 1970 00:00:01 GMT\r\nContent-Length: 0\r\n\r\n")
-                socket.close
+            await socket.sendBadRequestAndClose
 
 proc serve*(server: MicroAsyncHttpServer, port: Port, callback: proc (request: Request): Future[void] {.closure,gcsafe.},
             address = "") {.async.} =
--- microasynchttpserver.nimble
+++ microasynchttpserver.nimble
@@ -1,6 +1,6 @@
 [Package]
 
-version       = "0.9.0"
+version       = "0.9.5"
 author        = "Philip Wernersbach"
 description   = "A thin asynchronous HTTP server library, API compatible with Nim\'s built-in asynchttpserver."
 license       = "MIT"
